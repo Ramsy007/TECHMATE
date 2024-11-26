9839f0e83cb5c724356860fd937ca16039e286dc
Girdhar Mishra <girdharmishra10102003@gmail.com>
2024-11-26 14:15:49 +0530

    exploring crud operation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express =require("express");
  const app=express();
+  
+
+  
+ app.get("/user",(req,res)=>{
+    res.send("hii this is get call");
+ });
+
+ app.post("/user",(req,res)=>{
+    res.send("hii this is post call");
+ });
+
+
+
+ //this app.use will take all http method and return same result whatever the request you send 
+ app.use("/user",(req,res)=>{
+    res.send("hii from server")
+})
+
+ 
 
   app.use("/ss",(req,res)=>{
         res.send("hii from r")
   })
-
+ 
 
   app.listen(3000,()=>{
      console.log("server is running on  port 3000")
